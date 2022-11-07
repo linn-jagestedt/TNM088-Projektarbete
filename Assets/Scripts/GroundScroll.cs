9b77b00c5a6cc926f6117393e12d338433d71a52
Babbtox <albert.lannerstrom@gmail.com>
2022-11-07 10:21:17 +0100

    Added global "Options" game object
    Nu kan vi styra hastigheten från en "global variabel"

--- Assets/Scripts/GroundScroll.cs
+++ Assets/Scripts/GroundScroll.cs
@@ -4,13 +4,13 @@ using UnityEngine;
 
 public class GroundScroll : MonoBehaviour
 {
-    public float scrollSpeed;
+    public Options option;
 
     public float endX;
     public float startX;
 
     private void Update() {
-        transform.Translate(Vector2.left * scrollSpeed * Time.deltaTime);
+        transform.Translate(Vector2.left * option.speed * Time.deltaTime);
 
         if (transform.position.x <= endX) {
             Vector2 pos = new Vector2(startX, transform.position.y);
