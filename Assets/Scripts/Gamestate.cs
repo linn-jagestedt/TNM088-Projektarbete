cbc7d293b2a3a5b5b05c37b037a384f3c8ed5ef7
linn <linn.jagestedt@gmail>
2022-11-09 21:21:27 +0100

    Make speed based on time instead of frames

--- Assets/Scripts/Gamestate.cs
+++ Assets/Scripts/Gamestate.cs
@@ -8,27 +8,37 @@ public class Gamestate : MonoBehaviour
     // The speed at the start of the game
     public float InitialPlatformSpeed = 10;
 
+    // The amount of time before the speed increases
+    public float TimeUntilSpeedIncrease = 5;
+
+    // The amount to increase the speed every time step
+    public float SpeedIncrements = 5;
+
     // The current speed
-    public float CurrentPlatformSpeed { get => InitialPlatformSpeed * MathF.Round((_frameCounter / 1000.0f) + 1.0f); }
+    public float CurrentPlatformSpeed { 
+        get => InitialPlatformSpeed + SpeedIncrements * MathF.Floor((_seccondsPassed / TimeUntilSpeedIncrease)); 
+    }
  
     // Counts the number of frames since the game started
-    private long _frameCounter;
+    private float _seccondsPassed;
 
     // Start is called before the first frame update
     void Start()
     {
-        _frameCounter = 0;
+        _seccondsPassed = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
-        _frameCounter++;
+        // Add the secconds passed since last frame to _seccondsPassed
+        _seccondsPassed += Time.deltaTime;
+        Debug.Log($"_frameCounter: {_seccondsPassed}, CurrentPlatformSpeed: {CurrentPlatformSpeed}");
     }
 
     // Reset The Gamestate (new game)
     public void Reset()
     {
-        _frameCounter = 0;
+        _seccondsPassed = 0;
     }
 }
