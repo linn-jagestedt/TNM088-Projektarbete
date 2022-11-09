0ec71a0f88384a1176c3c9e3848e64eeb5bdd378
linn <linn.jagestedt@gmail>
2022-11-09 20:50:18 +0100

    Make PlatfromSpeed dependent on play time

--- Assets/Scripts/Gamestate.cs
+++ Assets/Scripts/Gamestate.cs
@@ -1,19 +1,34 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Gamestate : MonoBehaviour
 {
-    public float speed = 10;
+    // The speed at the start of the game
+    public float InitialPlatformSpeed = 10;
+
+    // The current speed
+    public float CurrentPlatformSpeed { get => InitialPlatformSpeed * MathF.Round((_frameCounter / 1000.0f) + 1.0f); }
+ 
+    // Counts the number of frames since the game started
+    private long _frameCounter;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        _frameCounter = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        _frameCounter++;
+    }
+
+    // Reset The Gamestate (new game)
+    public void Reset()
+    {
+        _frameCounter = 0;
     }
 }
--- Assets/Scripts/GroundScroll.cs
+++ Assets/Scripts/GroundScroll.cs
@@ -4,14 +4,14 @@ using UnityEngine;
 
 public class GroundScroll : MonoBehaviour
 {
-    public Gamestate state;
+    public Gamestate GameState;
 
     public float endX;
     public float startX;
 
     private void Update() {
         foreach (Transform child in transform) {
-            child.transform.Translate(Vector2.left * state.speed * Time.deltaTime);
+            child.transform.Translate(Vector2.left * GameState.CurrentPlatformSpeed * Time.deltaTime);
 
             if (child.transform.position.x < endX) {
                 float newX = startX - (endX - child.transform.position.x);
