847d83100707a82ee9f002e1212134e68000d094
Linn <linn.jagestedt@gmail.com>
2022-11-13 20:42:40 +0100

    Retriev component from script instead of scene

--- Assets/Scripts/JumpScript.cs
+++ Assets/Scripts/JumpScript.cs
@@ -4,20 +4,19 @@ using UnityEngine;
 
 public class JumpScript : MonoBehaviour
 {
-    public Rigidbody2D rb;
-
     public float jumpHeight;
+    private Rigidbody2D _rb;
 
     void Start()
     {
-        
+        _rb = GetComponent<Rigidbody2D>();
     }
 
     void Update()
     {
         if(Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.UpArrow)) {
             Debug.Log("jump");
-            rb.AddForce(Vector2.up*jumpHeight, ForceMode2D.Impulse);
+            _rb.AddForce(Vector2.up*jumpHeight, ForceMode2D.Impulse);
         }
     }
 }
