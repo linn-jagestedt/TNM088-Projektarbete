a2c7c851d60cafe662fb7ef8866859733da9bb9e
Linn <linn.jagestedt@gmail.com>
2022-11-13 14:42:29 +0100

    move velocity update to fixedupdate

--- Assets/Scripts/GroundScroll.cs
+++ Assets/Scripts/GroundScroll.cs
@@ -5,19 +5,22 @@ using UnityEngine;
 public class GroundScroll : MonoBehaviour
 {
     public GameState GameState;
-
     public float endX;
     public float startX;
 
     private void Update() {
         foreach (Transform child in transform) {
-            child.transform.Translate(Vector2.left * GameState.CurrentPlatformSpeed * Time.deltaTime);
-
             if (child.transform.position.x < endX) {
                 float newX = startX - (endX - child.transform.position.x);
                 Vector2 pos = new Vector2(newX, child.transform.position.y);
                 child.transform.position = pos;
             }
+        }
+    }
+
+    private void FixedUpdate() {
+        foreach (Rigidbody2D rb in GetComponentsInChildren<Rigidbody2D>()) {
+            rb.velocity = Vector2.left * GameState.CurrentPlatformSpeed;
         }
     }
 }
