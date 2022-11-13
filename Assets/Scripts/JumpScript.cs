7c50d2e6d34f97ca627d6b6376130c08c442a94d
Linn <linn.jagestedt@gmail.com>
2022-11-13 14:42:46 +0100

    remove comments

--- Assets/Scripts/JumpScript.cs
+++ Assets/Scripts/JumpScript.cs
@@ -8,13 +8,11 @@ public class JumpScript : MonoBehaviour
 
     public float jumpHeight;
 
-    // Start is called before the first frame update
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
         if(Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.UpArrow)) {
