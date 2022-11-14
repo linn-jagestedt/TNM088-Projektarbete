b11ce39a48548b402f577b324e561b6028f6c876
Linn <linn.jagestedt@gmail.com>
2022-11-14 21:21:21 +0100

    Add code for enemy colission

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class JumpScript : MonoBehaviour
+public class PlayerScript : MonoBehaviour
 {
     public float jumpHeight;
     private Rigidbody2D _rb;
@@ -17,6 +17,14 @@ public class JumpScript : MonoBehaviour
         if(Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.UpArrow)) {
             Debug.Log("jump");
             _rb.AddForce(Vector2.up*jumpHeight, ForceMode2D.Impulse);
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Enemy")
+        {
+            Debug.Log("Game Over");
         }
     }
 }
